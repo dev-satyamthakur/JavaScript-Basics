b8044648c60f9983fb78329ef5d5569cd4808d33
Neo <66131727+dev-satyamthakur@users.noreply.github.com>
2024-01-18 09:31:16 +0000

    Variables introduction

---
+++ varibles/variable_test.js
@@ -0,0 +1,8 @@
+const myName = "Neo"
+let myAge = 18
+myCity = "Faridabad"
+myCollege = "Lovely Professional University"
+
+// Don't use var. It has scope issues
+
+console.table([myName, myAge, myCity, myCollege])
